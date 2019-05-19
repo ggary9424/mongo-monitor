0e4daec26e6e865298bb91808f2df524c439cbd7
Gary Chen <gary@unh3o.com>
2019-05-19 18:12:49 +0800

    Enhance the UI of mongostat. Change the color and add more information to understand

--- termui/main.go
+++ termui/main.go
@@ -2,6 +2,7 @@ package termui
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	metricHelper "mongo-monitor/metric_helper"
@@ -9,13 +10,13 @@ import (
 	"github.com/mum4k/termdash"
 	"github.com/mum4k/termdash/cell"
 	"github.com/mum4k/termdash/container"
-	"github.com/mum4k/termdash/container/grid"
 	"github.com/mum4k/termdash/keyboard"
 	"github.com/mum4k/termdash/linestyle"
 	"github.com/mum4k/termdash/terminal/termbox"
 	"github.com/mum4k/termdash/terminal/terminalapi"
 	"github.com/mum4k/termdash/widgets/button"
 	"github.com/mum4k/termdash/widgets/linechart"
+	"github.com/mum4k/termdash/widgets/text"
 )
 
 // layoutButtons are buttons that change the layout.
@@ -25,8 +26,9 @@ type layoutButtons struct {
 
 // widgets holds the widgets used by this demo.
 type widgets struct {
-	opcountersLC *linechart.LineChart
-	buttons      *layoutButtons
+	mongostatUIText *text.Text
+	opcountersLC    *linechart.LineChart
+	opcountersText  *text.Text
 }
 
 // periodic executes the provided closure periodically every interval.
@@ -89,12 +91,13 @@ func extractOpcounters() (
 	return insertCountSlice, queryCountSlice, updateCountSlice, deleteCountSlice, getmoreCountSlice, commandCountSlice, XLabelMap
 }
 
-// newOpcounters returns a line chart that displays a heartbeat-like progression.
-func newOpcounters(ctx context.Context) (*linechart.LineChart, error) {
+// newOpcountersLc returns a line chart that displays opcounters line chart.
+func newOpcountersLc(ctx context.Context) (*linechart.LineChart, error) {
 	lc, err := linechart.New(
-		linechart.AxesCellOpts(cell.FgColor(cell.ColorRed)),
-		linechart.YLabelCellOpts(cell.FgColor(cell.ColorGreen)),
-		linechart.XLabelCellOpts(cell.FgColor(cell.ColorGreen)),
+		linechart.AxesCellOpts(cell.FgColor(cell.ColorNumber(161))),
+		linechart.YLabelCellOpts(cell.FgColor(cell.ColorNumber(222))),
+		linechart.XLabelCellOpts(cell.FgColor(cell.ColorNumber(222))),
+		linechart.XAxisUnscaled(),
 	)
 	if err != nil {
 		return nil, err
@@ -102,42 +105,42 @@ func newOpcounters(ctx context.Context) (*linechart.LineChart, error) {
 	go periodic(ctx, redrawInterval/3, func() error {
 		i, q, u, d, g, c, XLabelMap := extractOpcounters()
 		err := lc.Series("insert", i,
-			linechart.SeriesCellOpts(cell.FgColor(cell.ColorNumber(87))),
+			linechart.SeriesCellOpts(cell.FgColor(cell.ColorNumber(111))),
 			linechart.SeriesXLabels(XLabelMap),
 		)
 		if err != nil {
 			return err
 		}
 		err = lc.Series("query", q,
-			linechart.SeriesCellOpts(cell.FgColor(cell.ColorNumber(76))),
+			linechart.SeriesCellOpts(cell.FgColor(cell.ColorNumber(172))),
 			linechart.SeriesXLabels(XLabelMap),
 		)
 		if err != nil {
 			return err
 		}
 		err = lc.Series("update", u,
-			linechart.SeriesCellOpts(cell.FgColor(cell.ColorNumber(65))),
+			linechart.SeriesCellOpts(cell.FgColor(cell.ColorNumber(107))),
 			linechart.SeriesXLabels(XLabelMap),
 		)
 		if err != nil {
 			return err
 		}
 		err = lc.Series("delete", d,
-			linechart.SeriesCellOpts(cell.FgColor(cell.ColorNumber(54))),
+			linechart.SeriesCellOpts(cell.FgColor(cell.ColorNumber(161))),
 			linechart.SeriesXLabels(XLabelMap),
 		)
 		if err != nil {
 			return err
 		}
 		err = lc.Series("getmore", g,
-			linechart.SeriesCellOpts(cell.FgColor(cell.ColorNumber(43))),
+			linechart.SeriesCellOpts(cell.FgColor(cell.ColorNumber(245))),
 			linechart.SeriesXLabels(XLabelMap),
 		)
 		if err != nil {
 			return err
 		}
 		err = lc.Series("command", c,
-			linechart.SeriesCellOpts(cell.FgColor(cell.ColorNumber(32))),
+			linechart.SeriesCellOpts(cell.FgColor(cell.ColorNumber(135))),
 			linechart.SeriesXLabels(XLabelMap),
 		)
 		if err != nil {
@@ -148,37 +151,104 @@ func newOpcounters(ctx context.Context) (*linechart.LineChart, error) {
 	return lc, nil
 }
 
+// newMongostatChartText returns a text block that displays the basic infomation of mongostat chart.
+func newMongostatUIText(ctx context.Context) (*text.Text, error) {
+	t, err := text.New()
+	if err != nil {
+		return nil, err
+	}
+	if err := t.Write(
+		fmt.Sprintf("Press Esc/Q/Ctrl-C to quit\n"),
+		text.WriteCellOpts(cell.FgColor(cell.ColorNumber(111))),
+	); err != nil {
+		return nil, err
+	}
+
+	return t, nil
+}
+
+// newOpcountersText returns a text block that displays the infomation of opcounters line chart.
+func newOpcountersText(ctx context.Context) (*text.Text, error) {
+	t, err := text.New()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := t.Write(fmt.Sprintf("INSERT  .......\n"), text.WriteCellOpts(cell.FgColor(cell.ColorNumber(111)))); err != nil {
+		return nil, err
+	}
+	if err := t.Write(fmt.Sprintf("QUERY   .......\n"), text.WriteCellOpts(cell.FgColor(cell.ColorNumber(172)))); err != nil {
+		return nil, err
+	}
+	if err := t.Write(fmt.Sprintf("UPDATE  .......\n"), text.WriteCellOpts(cell.FgColor(cell.ColorNumber(107)))); err != nil {
+		return nil, err
+	}
+	if err := t.Write(fmt.Sprintf("DELETE  .......\n"), text.WriteCellOpts(cell.FgColor(cell.ColorNumber(161)))); err != nil {
+		return nil, err
+	}
+	if err := t.Write(fmt.Sprintf("GETMORE .......\n"), text.WriteCellOpts(cell.FgColor(cell.ColorNumber(245)))); err != nil {
+		return nil, err
+	}
+	if err := t.Write(fmt.Sprintf("COMMAND .......\n"), text.WriteCellOpts(cell.FgColor(cell.ColorNumber(135)))); err != nil {
+		return nil, err
+	}
+
+	return t, nil
+}
+
 // newWidgets creates all widgets used by this demo.
 func newWidgets(ctx context.Context, c *container.Container) (*widgets, error) {
-	opcountersLC, err := newOpcounters(ctx)
+	mongostatUIText, err := newMongostatUIText(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	opcountersLC, err := newOpcountersLc(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	opcountersText, err := newOpcountersText(ctx)
 	if err != nil {
 		return nil, err
 	}
 
 	return &widgets{
-		opcountersLC: opcountersLC,
+		mongostatUIText: mongostatUIText,
+		opcountersLC:    opcountersLC,
+		opcountersText:  opcountersText,
 	}, nil
 }
 
-// gridLayout prepares container options that represent the desired screen layout.
-// This function demonstrates the use of the grid builder.
-// gridLayout() and contLayout() demonstrate the two available layout APIs and
-// both produce equivalent layouts for layoutType layoutAll.
-func gridLayout(w *widgets) ([]container.Option, error) {
-	builder := grid.New()
-	builder.Add(
-		grid.ColWidthPerc(99, grid.RowHeightPerc(99, grid.Widget(w.opcountersLC,
-			container.Border(linestyle.Light),
-			container.BorderTitle("opcounters"),
-			container.BorderTitleAlignCenter(),
-		))),
-	)
-
-	gridOpts, err := builder.Build()
-	if err != nil {
-		return nil, err
-	}
-	return gridOpts, nil
+func getLayoutOpts(w *widgets) ([]container.Option, error) {
+	return []container.Option{
+		container.SplitHorizontal(
+			container.Top(
+				container.PlaceWidget(w.mongostatUIText),
+				container.Border(linestyle.Light),
+				container.BorderTitle("Welcome to Mongostat UI"),
+				container.BorderTitleAlignCenter(),
+			),
+			container.Bottom(
+				container.SplitVertical(
+					container.Left(
+						container.PlaceWidget(w.opcountersText),
+						container.Border(linestyle.Light),
+						container.BorderTitle("Lines"),
+						container.BorderTitleAlignCenter(),
+					),
+					container.Right(
+						container.PlaceWidget(w.opcountersLC),
+						container.Border(linestyle.Light),
+						container.BorderTitle("Opcounters Line Chart"),
+						container.BorderTitleAlignCenter(),
+					),
+					container.SplitPercent(15),
+				),
+			),
+			container.SplitPercent(12),
+		),
+	}, nil
 }
 
 const rootID = "root"
@@ -206,12 +276,12 @@ func Render(parentCtx context.Context) {
 		panic(err)
 	}
 
-	gridOpts, err := gridLayout(w) // equivalent to contLayout(w)
+	layoutOpts, err := getLayoutOpts(w)
 	if err != nil {
 		panic(err)
 	}
 
-	if err := c.Update(rootID, gridOpts...); err != nil {
+	if err := c.Update(rootID, layoutOpts...); err != nil {
 		panic(err)
 	}
 
